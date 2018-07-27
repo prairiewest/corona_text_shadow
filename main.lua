76ea3370bf9453974da3059f492398a83318ab97
Todd Trann <toddtrann@sasktel.net>
2018-07-26 22:41:00 -0600

    Change to two-phase rendering

--- main.lua
+++ main.lua
@@ -15,48 +15,91 @@ local function main()
 	local background = display.newImageRect("background.jpg",570,570)
 	background.x = display.contentWidth / 2
 	background.y = display.contentHeight / 2
+	local textObjects = {}
+	local animate, animate2
 
-	local myText1 = styledText.newText({
-		text = "Unstyled", 
+	animate = function()
+		transition.to( textObjects[5], { time=6000, y=textObjects[5].y+100, onComplete=animate2} )
+	end
+
+	animate2 = function()
+		transition.to( textObjects[5], { time=2000, rotation=360} )
+		textObjects[1]:removeSelf()  -- Show how to remove text
+	end
+
+	local registerObject = function(o)
+		table.insert(textObjects, o)
+		print("Registered text object #" .. #textObjects)
+		if (o.id) then print("id of this object: " .. o.id) end
+
+		if o.id and o.id == "text5" then
+			animate()
+		end
+	end
+
+	styledText.newText({
+		text = "Unstyled",
+		id = "text1",
 		textColor = {40,180,120,255},
 		x = display.contentWidth / 2,
 		y = spacing, 
 		font = "Arial", 
-		size = 40
+		size = 40,
+		callback = registerObject
 	})
 
-	local myText2 = styledText.newText({
-		text = "Glow Black", 
+	styledText.newText({
+		text = "Glow Black",
+		id = "text2",
 		textColor = {40,180,120,255},
 		x = display.contentWidth / 2,
 		y = spacing*2, 
 		font = "Arial", 
 		size = 40,
 		glowOffset = 1,
-		glowColor = {0,0,0,120}
+		glowColor = {0,0,0,120},
+		callback = registerObject
 	})
 
-	local myText3 = styledText.newText({
-		text = "Glow White", 
+	styledText.newText({
+		text = "Glow White",
+		id = "text3", 
 		textColor = {40,180,120,255},
 		x = display.contentWidth / 2,
 		y = spacing*3, 
 		font = "Arial", 
 		size = 40,
 		glowOffset = 1,
-		glowColor = {255,255,255,120}
+		glowColor = {255,255,255,120},
+		callback = registerObject
 	})
 
-	local myText4 = styledText.newText({
-		text = "Shadow Black", 
+	styledText.newText({
+		text = "Shadow Black",
+		id = "text4",
 		textColor = {40,180,120,255},
 		x = display.contentWidth / 2,
 		y = spacing*4, 
 		font = "Arial", 
 		size = 40,
 		shadowOffset = 3,
-		shadowColor = {0,0,0,180}
+		shadowColor = {0,0,0,180},
+		callback = registerObject
 	})
+
+	local myText5 = styledText.newText({
+		text = "White On Blue",
+		id = "text5",
+		textColor = {1,1,1,1.0},
+		x = display.contentWidth / 2,
+		y = spacing*5, 
+		font = "Arial", 
+		size = 40,
+		shadowOffset = 3,
+		shadowColor =  {0.3,0.3,1.0,0.8},
+		callback = registerObject
+	})
+
 
 end
 
--- styledText.lua
+++ styledText.lua
@@ -18,7 +18,7 @@
 --		glowColor = {120,0,0,180}
 --	})
 --
--- Colors can be specified using the new scale (0.0-1.0) or the old scale (0-255)
+-- Colors can be specified using floating point (0.0-1.0) or old style integer (0-255)
 --
 -- List of all parameters that can be passed in:
 --
@@ -45,47 +45,64 @@
 --
 --   embossDepth: integer, the number of pixels to raise or lower the emboss effect. Negative means raised instead. [default: 0]
 --
--- Note that to remove the text you must call its clear() function first:
---   myText:clear()
---   display:remove(myText)
+--   id: if supplied, the final object will be assigned this id
+--   callback: if supplied, this function will be called after the text is rendered, with the final output as the only argument
 --
-
+-- To remove the text once created, you must have kept a reference to it with the callback function:
+--   textObjectReference:removeSelf()
 
 
 local M = {}
 
 local function newText(options)
-    local labelText = {}, shadowOffset, shadowColor, glowOffset, glowColor, blurShadow, blurGlow
-    local text, size, font, textColor, x, y, embossDepth, width, align, anchorX, anchorY
-    local i = 1
+    local labelText = {}
+    local args = {
+    	align        = "left", 
+    	anchorX      = 0.5,
+    	anchorY      = 0.5,
+    	blurGlow     = 1,
+    	blurShadow   = 1, 
+    	callback    = nil,
+    	embossDepth  = 0, 
+    	font         = native.systemFontBold, 
+    	glowColor    = { 0.5,0.5,0.5,1.0 }, 
+    	glowOffset   = 0,
+    	shadowColor  = { 0, 0, 0, 0.5 }, 
+    	shadowOffset = 0, 
+    	size         = 24, 
+    	text         = " ",
+    	textColor    = { 1.0,1.0,1.0,1.0 }, 
+    	width        = nil,
+    	x            = display.contentWidth/2, 
+    	y            = display.contentHeight/2
+    }
+    local nextIndex = 1
 	local layer1 = display.newGroup()
-	layer1.x = display.contentWidth/2
-	layer1.y = display.contentHeight/2
+	layer1.x, layer1.y = display.contentWidth/2, display.contentHeight/2
 	local layer2 = display.newGroup()
-	layer2.x = display.contentWidth/2
-	layer2.y = display.contentHeight/2
-	local layerOutput = display.newGroup()
-	local output1, output2
+	layer2.x, layer2.y = display.contentWidth/2, display.contentHeight/2
 
-	if ( options.size and type(options.size) == "number" ) then size=options.size else size=24 end
-	if ( options.font ) then font=options.font else font=native.systemFontBold end
-	if ( options.x and type(options.x) == "number" ) then x=options.x else x=160 end
-	if ( options.y and type(options.y) == "number" ) then y=options.y else y=240 end
-	if ( options.anchorX and type(options.anchorX) == "number" ) then anchorX=options.anchorX end
-	if ( options.anchorY and type(options.anchorY) == "number" ) then anchorY=options.anchorY end
-	if ( options.width and type(options.width) == "number" ) then width=options.width end
-	if ( options.align) then align=options.align else align="left" end
-	if ( options.textColor ) then textColor=options.textColor else textColor={ 1.0,1.0,1.0,1.0 } end
-	if ( options.shadowColor ) then shadowColor=options.shadowColor else shadowColor={ 0, 0, 0, 0.5 } end
-	if ( options.shadowOffset and type(options.shadowOffset) == "number" ) then shadowOffset=options.shadowOffset else shadowOffset = 0 end
-	if ( options.glowColor ) then glowColor=options.glowColor else glowColor={ 0.5,0.5,0.5,1.0 } end
-	if ( options.glowOffset and type(options.glowOffset) == "number" ) then glowOffset=options.glowOffset else glowOffset = 0 end
-	if ( options.embossDepth and type(options.embossDepth) == "number" ) then embossDepth=options.embossDepth else embossDepth = 0 end
-	if ( options.blurGlow and type(options.blurGlow) == "number" ) then blurGlow=options.blurGlow else blurGlow = 1 end
-	if ( options.blurShadow and type(options.blurShadow) == "number" ) then blurShadow=options.blurShadow else blurShadow = 1 end
-	if ( options.text) then text = options.text else text = " " end
-	
-	local function applyNewColorScale(a)
+	if ( options.align        and type(options.align) == "string")         then args.align        = options.align end
+	if ( options.anchorX      and type(options.anchorX) == "number")       then args.anchorX      = options.anchorX end
+	if ( options.anchorY      and type(options.anchorY) == "number")       then args.anchorY      = options.anchorY end
+	if ( options.blurGlow     and type(options.blurGlow) == "number")      then args.blurGlow     = options.blurGlow end
+	if ( options.blurShadow   and type(options.blurShadow) == "number")    then args.blurShadow   = options.blurShadow end
+	if ( options.callback     and type(options.callback) == "function")    then args.callback     = options.callback end
+	if ( options.embossDepth  and type(options.embossDepth) == "number")   then args.embossDepth  = options.embossDepth end
+	if ( options.font )                                                    then args.font         = options.font end
+	if ( options.glowColor    and type(options.glowColor) == "table")      then args.glowColor    = options.glowColor end
+	if ( options.glowOffset   and type(options.glowOffset) == "number")    then args.glowOffset   = options.glowOffset end
+	if ( options.shadowColor  and type(options.shadowColor) == "table")    then args.shadowColor  = options.shadowColor end
+	if ( options.shadowOffset and type(options.shadowOffset) == "number")  then args.shadowOffset = options.shadowOffset end
+	if ( options.size         and type(options.size) == "number" )         then args.size         = options.size end
+	if ( options.text         and type(options.text) == "string")          then args.text         = options.text end
+	if ( options.id           and type(options.id) == "string")            then args.id           = options.id end
+	if ( options.textColor    and type(options.textColor) == "table")      then args.textColor    = options.textColor end
+	if ( options.width        and type(options.width) == "number" )        then args.width        = options.width end
+	if ( options.x            and type(options.x) == "number" )            then args.x            = options.x end
+	if ( options.y            and type(options.y) == "number" )            then args.y            = options.y end
+
+	local function applyColorScale(a)
 		if type(a[1]) == "number" then
 			if (a[1] > 1.0 or a[2] > 1.0 or a[3] > 1.0 or a[4] > 1.0) then
 				a[1] = a[1] / 255
@@ -95,179 +112,148 @@ local function newText(options)
 			end
 		end 
 	end
-		
-	applyNewColorScale(textColor)
-	applyNewColorScale(shadowColor)
-	applyNewColorScale(glowColor)
+
+	applyColorScale(args.textColor)
+	applyColorScale(args.shadowColor)
+	applyColorScale(args.glowColor)
 	
-	if (embossDepth ~= 0) then shadowOffset = 0; glowOffset = 0;textColor={ 0.5,0.5,0.5,1.0 } end
-	if (glowOffset == 1) then glowOffset = 1.5 end -- compensate for loss due to blur
-	
-	if (shadowOffset > 0) then
-		if (glowOffset > 0) then
-			for xo = -glowOffset,glowOffset,1 do
-				for yo = -glowOffset,glowOffset,1 do
-					local style = {
-						parent=layer1,
-						text=text, 
-						font=font, 
-						fontSize=size
-					}
-					if (type(width) == "number") then
-						style.width = width
-						style.align = align
-					end
-					labelText[i] = display.newText(style)
-					labelText[i].x = shadowOffset+xo; labelText[i].y = shadowOffset+yo
-					labelText[i]:setFillColor( shadowColor[1], shadowColor[2], shadowColor[3], shadowColor[4] )
-					i = i + 1
+	if (args.embossDepth ~= 0) then args.shadowOffset = 0; args.glowOffset = 0; args.textColor={ 0.5,0.5,0.5,1.0 } end
+	if (args.glowOffset == 1) then args.glowOffset = 1.5 end -- compensate for loss due to blur
+	local style1 = {
+		parent   = layer1,
+		text     = args.text,
+		font     = args.font, 
+		fontSize = args.size
+	}
+	local style2 = {
+		parent   = layer2,
+		text     = args.text,
+		font     = args.font, 
+		fontSize = args.size
+	}
+	if (type(args.width) == "number") then
+		style1.width = args.width; style2.width = args.width
+		style1.align = args.align; style2.align = args.align
+	end
+
+	if (args.shadowOffset > 0) then
+		if (args.glowOffset > 0) then
+			for xo = -args.glowOffset,args.glowOffset,1 do
+				for yo = -args.glowOffset,args.glowOffset,1 do
+					labelText[nextIndex] = display.newText(style1)
+					labelText[nextIndex].x = args.shadowOffset+xo; labelText[nextIndex].y = args.shadowOffset+yo
+					labelText[nextIndex]:setFillColor( args.shadowColor[1], args.shadowColor[2], args.shadowColor[3], args.shadowColor[4] )
+					nextIndex = nextIndex + 1
 				end 
 			end
 		else
-			local style = {
-				parent=layer1,
-				text=text,
-				font=font, 
-				fontSize=size
-			}
-			if (type(width) == "number") then
-				style.width = width
-				style.align = align
-			end
-			labelText[i] = display.newText(style)
-			labelText[i].x = shadowOffset; labelText[i].y = shadowOffset
-			labelText[i]:setFillColor( shadowColor[1], shadowColor[2], shadowColor[3], shadowColor[4] )
-			i = i + 1
+			labelText[nextIndex] = display.newText(style1)
+			labelText[nextIndex].x = args.shadowOffset; labelText[nextIndex].y = args.shadowOffset
+			labelText[nextIndex]:setFillColor( args.shadowColor[1], args.shadowColor[2], args.shadowColor[3], args.shadowColor[4] )
+			nextIndex = nextIndex + 1
 		end
 	end
 
-	if (embossDepth < 0) then
-		for yo = -embossDepth,embossDepth,embossDepth do
-			local style = {
-				parent=layer1,
-				text=text, 
-				font=font, 
-				fontSize=size
-			}
-			if (type(width) == "number") then
-				style.width = width
-				style.align = align
-			end
-			labelText[i] = display.newText(style)
-			labelText[i].x = 0; labelText[i].y = yo
+	if (args.embossDepth < 0) then
+		for yo = -args.embossDepth,args.embossDepth,args.embossDepth do
+			labelText[nextIndex] = display.newText(style1)
+			labelText[nextIndex].x = 0; labelText[nextIndex].y = yo
 			if (yo < 0) then 
-				labelText[i]:setFillColor( 0, 0, 0, 80 )
+				labelText[nextIndex]:setFillColor( 0, 0, 0, 80 )
 			else 
-				labelText[i]:setFillColor( 255,255,255,80 )
+				labelText[nextIndex]:setFillColor( 255,255,255,80 )
 			end
-			i = i + 1
+			nextIndex = nextIndex + 1
 		end
-	elseif (embossDepth > 0) then
-		for yo = embossDepth,-embossDepth,-embossDepth do
-			local style = {
-				parent=layer1,
-				text=text, 
-				font=font, 
-				fontSize=size 
-			}
-			if (type(width) == "number") then
-				style.width = width
-				style.align = align
-			end
-			labelText[i] = display.newText(style)
-			labelText[i].x = 0; labelText[i].y = yo
+	elseif (args.embossDepth > 0) then
+		for yo = args.embossDepth,-args.embossDepth,-args.embossDepth do
+			labelText[nextIndex] = display.newText(style1)
+			labelText[nextIndex].x = 0; labelText[nextIndex].y = yo
 			if (yo < 0) then 
-				labelText[i]:setFillColor( 255,255,255,80)
+				labelText[nextIndex]:setFillColor( 255,255,255,80)
 			else 
-				labelText[i]:setFillColor( 0,0,0,80 )
+				labelText[nextIndex]:setFillColor( 0,0,0,80 )
 			end
-			i = i + 1
+			nextIndex = nextIndex + 1
 		end
 	end
 
-	if (glowOffset > 0) then
-		for xo = -glowOffset,glowOffset,1 do
-			for yo = -glowOffset,glowOffset,1 do
-				local style = {
-					parent=layer2,
-					text=text, 
-					font=font, 
-					fontSize=size
-				}
-				if (type(width) == "number") then
-					style.width = width
-					style.align = align
-				end
-				labelText[i] = display.newText(style)
-				labelText[i].x = xo; labelText[i].y = yo
-				labelText[i]:setFillColor( glowColor[1], glowColor[2], glowColor[3], glowColor[4] )
-				i = i + 1
+	if (args.glowOffset > 0) then
+		for xo = -args.glowOffset,args.glowOffset,1 do
+			for yo = -args.glowOffset,args.glowOffset,1 do
+				labelText[nextIndex] = display.newText(style2)
+				labelText[nextIndex].x = xo; labelText[nextIndex].y = yo
+				labelText[nextIndex]:setFillColor( args.glowColor[1], args.glowColor[2], args.glowColor[3], args.glowColor[4] )
+				nextIndex = nextIndex + 1
 			end 
 		end
 	end
-	
-	if (shadowOffset > 1 or embossDepth ~= 0) then
-		output1 = display.capture(layer1)
-		if (blurShadow == 1) then
-			output1.fill.effect = "filter.blurGaussian"
-			output1.fill.effect.horizontal.blurSize = 2
-			output1.fill.effect.horizontal.sigma = 1.5
-			output1.fill.effect.vertical.blurSize = 2
-			output1.fill.effect.vertical.sigma = 1.5
-		end
-		output1.x = shadowOffset
-		output1.y = shadowOffset
-		layerOutput:insert(output1)
-	end
-	if (glowOffset > 1) then
-		output2 = display.capture(layer2)
-		if (blurGlow == 1) then
-			output2.fill.effect = "filter.blurGaussian"
-			output2.fill.effect.horizontal.blurSize = 2
-			output2.fill.effect.horizontal.sigma = 1.5
-			output2.fill.effect.vertical.blurSize = 2
-			output2.fill.effect.vertical.sigma = 1.5
-		end
-		layerOutput:insert(output2)
-	end
-	for i, v in ipairs(labelText) do
-		labelText[i]:removeSelf()
-		labelText[i] = nil
-	end
-	layer1:removeSelf(); layer1 = nil
-	layer2:removeSelf(); layer2 = nil
-	
-	local style = {
-		parent=layerOutput,
-		text=text, 
-		font=font, 
-		fontSize=size
-	}
-	if (type(width) == "number") then
-		style.width = width
-		style.align = align
-	end
-	labelText[i] = display.newText(style)
-	labelText[i].x = 0; labelText[i].y = 0
-	labelText[i]:setFillColor( textColor[1], textColor[2], textColor[3], textColor[4] )
-	i = i + 1
-	if (type(anchorX) == "number") then
-		layerOutput.anchorX = anchorX
-	end
-	if (type(anchorY) == "number") then
-		layerOutput.anchorY = anchorY
-	end
-	layerOutput.x=x; layerOutput.y=y
 
-	function layerOutput:clear()
-		for i=1,layerOutput.numChildren do
-			if (layerOutput[i] ~= nil) then
-			    layerOutput[i]:removeSelf()
+	local startCapture = function(args, layer1, layer2, labelText)
+		local layerOutput = display.newGroup()
+		local output1, output2
+
+		if (args.shadowOffset > 1 or args.embossDepth ~= 0) then
+			output1 = display.capture(layer1)
+			if (args.blurShadow == 1) then
+				output1.fill.effect = "filter.blurGaussian"
+				output1.fill.effect.horizontal.blurSize = 2
+				output1.fill.effect.horizontal.sigma = 1.5
+				output1.fill.effect.vertical.blurSize = 2
+				output1.fill.effect.vertical.sigma = 1.5
 			end
+			output1.x = args.shadowOffset
+			output1.y = args.shadowOffset
+			layerOutput:insert(output1)
 		end
-	end
+
+		if (args.glowOffset > 1) then
+			output2 = display.capture(layer2)
+			if (args.blurGlow == 1) then
+				output2.fill.effect = "filter.blurGaussian"
+				output2.fill.effect.horizontal.blurSize = 2
+				output2.fill.effect.horizontal.sigma = 1.5
+				output2.fill.effect.vertical.blurSize = 2
+				output2.fill.effect.vertical.sigma = 1.5
+			end
+			layerOutput:insert(output2)
+		end
+		for i, v in ipairs(labelText) do
+			labelText[i]:removeSelf()
+			labelText[i] = nil
+		end
+		layer1:removeSelf(); layer1 = nil
+		layer2:removeSelf(); layer2 = nil
 		
-	return layerOutput
+		local styleO = {
+			parent=layerOutput,
+			text=args.text, 
+			font=args.font, 
+			fontSize=args.size
+		}
+		if (type(args.width) == "number") then
+			styleO.width = args.width; styleO.align = args.align
+		end
+		labelText[nextIndex] = display.newText(styleO)
+		labelText[nextIndex].x = 0; labelText[nextIndex].y = 0
+		labelText[nextIndex]:setFillColor( args.textColor[1], args.textColor[2], args.textColor[3], args.textColor[4] )
+		nextIndex = nextIndex + 1
+		if (type(args.anchorX) == "number") then
+			layerOutput.anchorX = args.anchorX
+		end
+		if (type(args.anchorY) == "number") then
+			layerOutput.anchorY = args.anchorY
+		end
+		layerOutput.x=args.x; layerOutput.y=args.y
+
+		if args.id then layerOutput.id = args.id end
+		if args.callback then args.callback(layerOutput) end
+	end
+
+  	timer.performWithDelay( 100, function()
+  		return startCapture(args, layer1, layer2, labelText)
+  	end)
+
 end
 M.newText = newText
 
